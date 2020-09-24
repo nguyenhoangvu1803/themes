0bd53f3c18c9efce8d88d91b4efe2424021cb478
DESKTOP-9BOOVPI\Admin <nguyenhoangvu1803@gmail.com>
2020-09-24 16:28:47 +0700

    up

--- flatsome/woocommerce/single-product/add-to-cart/variation-add-to-cart-button.php
+++ flatsome/woocommerce/single-product/add-to-cart/variation-add-to-cart-button.php
@@ -39,110 +39,11 @@ global $product;
 </div>
 
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-	<style>
-	.flag {
-    display: table;
-    width: 100%;
-    margin-bottom: -15px;
-  }
-    .vertical-align-top {
-    vertical-align: top;
-  }
-    .flag-body,.flag-img {
-    display: table-cell;
-    vertical-align: middle;
-  }
-
-#hourglass {
-  opacity: 1;
-  color: #6a1b9a;
-  font-size: 3rem;
-}
-#hourglass i {
-  opacity: 0;
-  animation: hourglass 4.8s ease-in infinite, hourglass-spin 4.8s ease-out infinite;
-}
-#hourglass > i:nth-child(1) {
-  /* color: #ffc400; */
-  color:#53b802;
-  animation-delay: 0s, 0s;
-}
-#hourglass > i:nth-child(2) {
-  color: #53b802;
-  animation-delay: 1.2s, 0s;
-}
-#hourglass > i:nth-child(3) {
-  color: #53b802;
-  animation-delay: 2.4s, 0s;
-}
-#hourglass > i:nth-child(4) {
-  color: #53b802;
-  animation-delay: 3.6s, 0s;
-}
-#hourglass > i:nth-child(4) {
-  animation: hourglass-end 4.8s ease-in infinite, hourglass-spin 4.8s ease-out infinite;
-}
-#hourglass > i:nth-child(5) {
-  color: #53b802;
-  opacity: 1;
-  animation: hourglass-spin 4.8s ease-out infinite;
-}
-
-.fa-hourglass-o:before {
-    color: #bbbec4;
-}
-
-@keyframes hourglass {
-  0% {
-    opacity: 1;
-  }
-  24% {
-    opacity: 0.9;
-  }
-  26% {
-    opacity: 0;
-  }
-}
-@keyframes hourglass-end {
-  0% {
-    opacity: 0;
-  }
-  70% {
-    opacity: 0;
-  }
-  75% {
-    opacity: 1;
-  }
-  100% {
-    opacity: 1;
-  }
-}
-@keyframes hourglass-spin {
-  75% {
-    transform: rotate(0deg);
-  }
-  100% {
-    transform: rotate(180deg);
-  }
-}
-
-	</style>
-  <div id="hourglasswraper" class="flag">
-    <!-- <div class="flag-img">
-      	<div id="hourglass" class="fa-stack fa-4x">
-          <i class="fa fa-stack-1x fa-hourglass-start"></i>
-          <i class="fa fa-stack-1x fa-hourglass-half"></i>
-          <i class="fa fa-stack-1x fa-hourglass-end"></i>
-          <i class="fa fa-stack-1x fa-hourglass-end"></i>
-          <i class="fa fa-stack-1x fa-hourglass-o"></i>
-        </div>
-    </div> -->
-  </div>
   <?php $pieces = rand(5, 15); ?>
-  <div class="flag-body">
-    <img src="<?php echo get_template_directory_uri() . '/assets/img/almost.svg';?>" alt="" /><span style="font-weight: bold">Almost gone.</span> There are only <?php  echo rand(2, 5); ?> left.
+  <div class="flag-body" style="font-size: 15px;color: #000;">
+    <img src="<?php echo get_template_directory_uri() . '/assets/img/almost.svg';?>" alt="" /><span style="font-weight: 400;">Almost gone.</span> <span style="font-weight: 500;">There are only <?php  echo rand(2, 5); ?> left.</span>
   </div>
-  <div>This <?php echo date('F'); ?>, we only do <?php echo $pieces; ?> pieces of this limited edition. <span style="font-weight: bold;">Get it before it's gone!</span></div>
+  <div style="font-size: 13px;color: #4F4F4F;">This <?php echo date('F'); ?>, we only do <?php echo $pieces; ?> pieces of this limited edition. <span style="font-weight: 500;color: #000;">Get it before it's gone!</span></div>
   <div>
   <img  alt="credit cards" src="https://flagwix.com/wp-content/uploads/2020/09/checkout-opt.png">
   <img src="https://flagwix.com/wp-content/uploads/2020/09/product.jpg">
