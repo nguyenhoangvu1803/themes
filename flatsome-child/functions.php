a461ae62854b67e5900bd7cb6835aa52dd9a4212
DESKTOP-9BOOVPI\Admin <nguyenhoangvu1803@gmail.com>
2020-09-24 10:43:40 +0700

    up

--- flatsome-child/functions.php
+++ flatsome-child/functions.php
@@ -207,9 +207,14 @@ function woocommerce_custom_sales_price( $price, $regular_price, $sale_price ) {
 
   // Just for variable products on single product pages
   if( $product instanceof WC_Product && $product->is_type('variable') && is_product() ) {
-    $currency_symbol = get_woocommerce_currency_symbol();
-    $saved = $regular_price - $sale_price;
-    return '<del>' . wc_price( $regular_price ) . '</del><span class="product_saving_amount"> SAVE ' . $currency_symbol . number_format($saved, 2, '.', '') . '</span><ins>' . wc_price( $sale_price ) . '</ins>';
+    // $currency_symbol = get_woocommerce_currency_symbol();
+    // $saved = $regular_price - $sale_price;
+    $percentage = ( $regular_price - $sale_price ) / $regular_price * 100;
+    return '
+        <ins>' . wc_price( $sale_price ) . '</ins>
+        <del>' . wc_price( $regular_price ) . '</del>
+        <span class="product_saving_amount"> You saved ' . $percentage . '% this time</span>
+    ';
   }
 
   return $price;
--- flatsome/woocommerce/single-product/add-to-cart/variable.php
+++ flatsome/woocommerce/single-product/add-to-cart/variable.php
@@ -54,7 +54,7 @@ do_action( 'woocommerce_before_add_to_cart_form' ); ?>
 		</table>
 
 		<div class="single_variation_wrap">
-      <div style="font-size: 0.88em;">This is the best offer you've ever seen, <span style="font-weight: 700;">only this <?php echo date('F'); ?></span></div>
+      <div class="font-size: 13px; color: #828282;">This is the best offer you've ever seen, <span style="font-weight: 700; color: #000;">only this <?php echo date('F'); ?></span></div>
 			<?php
 				/**
 				 * Hook: woocommerce_before_single_variation.
