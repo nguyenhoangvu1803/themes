f3de033da3a6e537cbb30c67a45533425c213b28
vubjn <nguyenhoangvu1803@gmail.com>
2020-09-28 10:28:08 +0700

    cart

--- flatsome/woocommerce/cart/cart.php
+++ flatsome/woocommerce/cart/cart.php
@@ -288,12 +288,14 @@ do_action( 'woocommerce_before_cart' ); ?>
 
 	<div class="cart-collaterals">
 
+		<h4>Cart Total</h4>
+
 		<div class="cart-sidebar col-inner <?php echo $sidebar_classes; ?>">
 			<?php if ( wc_coupons_enabled() ) { ?>
 			<form class="checkout_coupon mb-0" method="post">
 				<div class="coupon">
-	        <h3 class="widget-title togglecoupon"><?php echo get_flatsome_icon( 'icon-tag' ); ?> <?php esc_html_e( 'Have a coupon? Click here', 'woocommerce' ); ?></h3>
-	        <div class="cart-coupon-wrapper" style="display: none;">
+	        		<h3 class="widget-title togglecoupon"><?php echo get_flatsome_icon( 'icon-tag' ); ?> <?php esc_html_e( 'Have a coupon? Click here', 'woocommerce' ); ?></h3>
+	        		<div class="cart-coupon-wrapper" style="display: none;">
 						<input type="text" name="coupon_code" class="input-text" id="coupon_code" value="" placeholder="<?php esc_attr_e( 'Coupon code', 'woocommerce' ); ?>" /> <input type="submit" class="is-form expand" name="apply_coupon" value="<?php esc_attr_e( 'Apply coupon', 'woocommerce' ); ?>" />
 						<?php do_action( 'woocommerce_cart_coupon' ); ?>
 					</div>
@@ -319,15 +321,15 @@ do_action( 'woocommerce_before_cart' ); ?>
 				do_action( 'woocommerce_cart_collaterals' );
 			?>
 			<?php if ( wc_coupons_enabled() ) { ?>
-			<form class="checkout_coupon mb-0" method="post">
-				<div class="coupon">
-					<h3 class="widget-title"><?php echo get_flatsome_icon( 'icon-tag' ); ?> <?php esc_html_e( 'Coupon', 'woocommerce' ); ?></h3><input type="text" name="coupon_code" class="input-text" id="coupon_code" value="" placeholder="<?php esc_attr_e( 'Coupon code', 'woocommerce' ); ?>" /> <input type="submit" class="is-form expand" name="apply_coupon" value="<?php esc_attr_e( 'Apply coupon', 'woocommerce' ); ?>" />
-					<?php do_action( 'woocommerce_cart_coupon' ); ?>
-				</div>
-			</form>
+				<form class="checkout_coupon mb-0" method="post">
+					<div class="coupon">
+						<h3 class="widget-title"><?php echo get_flatsome_icon( 'icon-tag' ); ?> <?php esc_html_e( 'Coupon', 'woocommerce' ); ?></h3><input type="text" name="coupon_code" class="input-text" id="coupon_code" value="" placeholder="<?php esc_attr_e( 'Coupon code', 'woocommerce' ); ?>" /> <input type="submit" class="is-form expand" name="apply_coupon" value="<?php esc_attr_e( 'Apply coupon', 'woocommerce' ); ?>" />
+						<?php do_action( 'woocommerce_cart_coupon' ); ?>
+					</div>
+				</form>
 			<?php } ?>
 		</div>
-		
+
 	</div>
 
 	<?php do_action( 'flatsome_cart_sidebar' ); ?>
