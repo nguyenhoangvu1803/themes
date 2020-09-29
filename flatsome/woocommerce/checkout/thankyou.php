8459860c67bb376c17b382d98cb675baa36b1142
vubjn <nguyenhoangvu1803@gmail.com>
2020-09-29 15:22:55 +0700

    checkout

--- flatsome/woocommerce/checkout/thankyou.php
+++ flatsome/woocommerce/checkout/thankyou.php
@@ -24,31 +24,45 @@ defined( 'ABSPATH' ) || exit;
 
 		do_action( 'woocommerce_before_thankyou', $order->get_id() ); ?>
 
-		<?php if ( $order->has_status( 'failed' ) ) : ?>
-		<div class="large-12 col order-failed">
-			<p class="woocommerce-notice woocommerce-notice--error woocommerce-thankyou-order-failed"><?php esc_html_e( 'Unfortunately your order cannot be processed as the originating bank/merchant has declined your transaction. Please attempt your purchase again.', 'woocommerce' ); ?></p>
+		<h2>Order Complete</h2>
 
-			<p class="woocommerce-notice woocommerce-notice--error woocommerce-thankyou-order-failed-actions">
-				<a href="<?php echo esc_url( $order->get_checkout_payment_url() ); ?>" class="button pay"><?php esc_html_e( 'Pay', 'woocommerce' ); ?></a>
-				<?php if ( is_user_logged_in() ) : ?>
-					<a href="<?php echo esc_url( wc_get_page_permalink( 'myaccount' ) ); ?>" class="button pay"><?php esc_html_e( 'My account', 'woocommerce' ); ?></a>
-				<?php endif; ?>
-			</p>
-		</div>
+		<?php if ( $order->has_status( 'failed' ) ) : ?>
+
+			<div class="large-12 col order-failed">
+				<p class="woocommerce-notice woocommerce-notice--error woocommerce-thankyou-order-failed"><?php esc_html_e( 'Unfortunately your order cannot be processed as the originating bank/merchant has declined your transaction. Please attempt your purchase again.', 'woocommerce' ); ?></p>
+
+				<p class="woocommerce-notice woocommerce-notice--error woocommerce-thankyou-order-failed-actions">
+					<a href="<?php echo esc_url( $order->get_checkout_payment_url() ); ?>" class="button pay"><?php esc_html_e( 'Pay', 'woocommerce' ); ?></a>
+					<?php if ( is_user_logged_in() ) : ?>
+						<a href="<?php echo esc_url( wc_get_page_permalink( 'myaccount' ) ); ?>" class="button pay"><?php esc_html_e( 'My account', 'woocommerce' ); ?></a>
+					<?php endif; ?>
+				</p>
+			</div>
 
 		<?php else : ?>
-    <div class="large-7 col">
 
-    <?php
-    $get_payment_method = $order->get_payment_method();
-    $get_order_id       = $order->get_id();
-    ?>
-    <?php do_action( 'woocommerce_thankyou_' . $get_payment_method, $get_order_id ); ?>
-    <?php do_action( 'woocommerce_thankyou', $get_order_id ); ?>
+	    <div class="large-7 col">
 
-    </div>
+	    	<div class="notice-order-success">
+	    		<svg width="53" height="53" viewBox="0 0 53 53" fill="none" xmlns="http://www.w3.org/2000/svg">
+				<circle cx="26.5" cy="26.5" r="26.5" fill="#53A42F"/>
+				<path d="M14 26.1538L23.5238 36L39 20" stroke="white" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
+				</svg>
+				<div>Thank you. Your order has been received.</div>
+				<div>A copy of your receipt has been sent to: <span><?php echo $order->get_billing_email(); ?></span></div>
+	    	</div>
 
-		<div class="large-5 col">
+		    <?php
+		    	$get_payment_method = $order->get_payment_method();
+		    	$get_order_id       = $order->get_id();
+		    ?>
+		    <?php do_action( 'woocommerce_thankyou_' . $get_payment_method, $get_order_id ); ?>
+		    <?php do_action( 'woocommerce_thankyou', $get_order_id ); ?>
+	    </div>
+
+	    <div class="large-1 col"></div>
+
+		<div class="large-4 col">
 			<div class="is-well col-inner entry-content">
 				<p class="success-color woocommerce-notice woocommerce-notice--success woocommerce-thankyou-order-received"><strong><?php echo apply_filters( 'woocommerce_thankyou_order_received_text', esc_html__( 'Thank you. Your order has been received.', 'woocommerce' ), $order ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></strong></p>
 
