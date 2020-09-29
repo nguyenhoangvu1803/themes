c539f39c1bfd7a6bdc661feba0e70818db1c6e87
vubjn <nguyenhoangvu1803@gmail.com>
2020-09-29 15:50:26 +0700

    checkout

--- flatsome/woocommerce/checkout/thankyou.php
+++ flatsome/woocommerce/checkout/thankyou.php
@@ -65,29 +65,29 @@ defined( 'ABSPATH' ) || exit;
 		<div class="large-4 col">
 			<div class="is-well col-inner entry-content">
 				
-				<p class="success-color woocommerce-notice woocommerce-notice--success woocommerce-thankyou-order-received"><strong><?php echo apply_filters( 'woocommerce_thankyou_order_received_text', esc_html__( 'Thank you. Your order has been received.', 'woocommerce' ), $order ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></strong></p>
+				<p class="woocommerce-notice woocommerce-notice--success woocommerce-thankyou-order-received"><?php echo apply_filters( 'woocommerce_thankyou_order_received_text', esc_html__( 'Thank you. Your order has been received.', 'woocommerce' ), $order ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
 
 				<ul class="woocommerce-order-overview woocommerce-thankyou-order-details order_details">
 
 					<li class="woocommerce-order-overview__order order">
-						<?php esc_html_e( 'Order number:', 'woocommerce' ); ?>
+						<lable><?php esc_html_e( 'Order number:', 'woocommerce' ); ?></lable>
 						<strong><?php echo $order->get_order_number(); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></strong>
 					</li>
 
 						<li class="woocommerce-order-overview__date date">
-							<?php esc_html_e( 'Date:', 'woocommerce' ); ?>
+							<lable><?php esc_html_e( 'Date:', 'woocommerce' ); ?></lable>
 							<strong><?php echo wc_format_datetime( $order->get_date_created() ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></strong>
 						</li>
 
 						<?php if ( is_user_logged_in() && $order->get_user_id() === get_current_user_id() && $order->get_billing_email() ) : ?>
 							<li class="woocommerce-order-overview__email email">
-								<?php esc_html_e( 'Email:', 'woocommerce' ); ?>
+								<lable><?php esc_html_e( 'Email:', 'woocommerce' ); ?></lable>
 								<strong><?php echo $order->get_billing_email(); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></strong>
 							</li>
 						<?php endif; ?>
 
 					<li class="woocommerce-order-overview__total total">
-						<?php esc_html_e( 'Total:', 'woocommerce' ); ?>
+						<lable><?php esc_html_e( 'Total:', 'woocommerce' ); ?></lable>
 						<strong><?php echo $order->get_formatted_order_total(); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></strong>
 					</li>
 
@@ -96,7 +96,7 @@ defined( 'ABSPATH' ) || exit;
 					if ( $payment_method_title ) :
 					?>
 						<li class="woocommerce-order-overview__payment-method method">
-							<?php esc_html_e( 'Payment method:', 'woocommerce' ); ?>
+							<lable><?php esc_html_e( 'Payment method:', 'woocommerce' ); ?></lable>
 							<strong><?php echo wp_kses_post( $payment_method_title ); ?></strong>
 						</li>
 					<?php endif; ?>
