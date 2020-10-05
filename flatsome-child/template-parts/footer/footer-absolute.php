bd4b2cdd23c0384533807a592333fe456f103281
vubjn <nguyenhoangvu1803@gmail.com>
2020-10-05 10:27:27 +0700

    widget

--- flatsome-child/template-parts/footer/footer-absolute.php
+++ flatsome-child/template-parts/footer/footer-absolute.php
@@ -12,12 +12,6 @@ $flatsome_footer_right_text = trim( get_theme_mod( 'footer_right_text' ) );
 
 ?>
 
-<div class="section-content relative">
-    <div>
-      asdasldj;alsd;laksd
-    </div>
-</div>
-
 <div class="absolute-footer <?php echo flatsome_option('footer_bottom_text'); ?> medium-text-center <?php echo $align;?> hide-for-small">
   
   <div class="container clearfix">
