abfe5fd444c7f21035cab19d9bb8e97789abc0a2
vubjn <nguyenhoangvu1803@gmail.com>
2020-10-13 14:25:30 +0700

    reviews

--- flatsome-child/functions.php
+++ flatsome-child/functions.php
@@ -382,7 +382,7 @@ function uoy_custom_css() {
         if($color_primary && $color_primary !== Flatsome_Default::COLOR_PRIMARY){
       ?>
           /* COLOR */
-          .color-primary, .fl-wrap.fl-is-active>label[for]:first-child, .fl-wrap.fl-has-focus>label[for]:first-child, form .woocommerce-form-row:hover label, a.lost_password, .lost_password a, .nav-right>li.cart-item>a, .product-info .posted_in a, ul.product-tabs li > a
+          .color-primary, .fl-wrap.fl-is-active>label[for]:first-child, .fl-wrap.fl-has-focus>label[for]:first-child, form .woocommerce-form-row:hover label, a.lost_password, .lost_password a, .nav-right>li.cart-item>a, .product-info .posted_in a, ul.product-tabs li > a, ul.product-tabs .star
           {color: <?php echo $color_primary; ?>;}
 
           /* BACKGROUND-COLOR */
