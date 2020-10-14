4a3d1478c5af639005627ff2e3f9305274f32212
vubjn <nguyenhoangvu1803@gmail.com>
2020-10-14 10:01:03 +0700

    add coupon on checkout review

--- flatsome-child/woocommerce/cart/cart.php
+++ flatsome-child/woocommerce/cart/cart.php
@@ -325,14 +325,6 @@ do_action( 'woocommerce_before_cart' ); ?>
 				 */
 				do_action( 'woocommerce_cart_collaterals' );
 			?>
-			<?php if ( wc_coupons_enabled() ) { ?>
-				<form class="checkout_coupon mb-0" method="post">
-					<div class="coupon">
-						<h3 class="widget-title"><?php echo get_flatsome_icon( 'icon-tag' ); ?> <?php esc_html_e( 'Coupon', 'woocommerce' ); ?></h3><input type="text" name="coupon_code" class="input-text" id="coupon_code" value="" placeholder="<?php esc_attr_e( 'Coupon code', 'woocommerce' ); ?>" /> <input type="submit" class="is-form expand" name="apply_coupon" value="<?php esc_attr_e( 'Apply coupon', 'woocommerce' ); ?>" />
-						<?php do_action( 'woocommerce_cart_coupon' ); ?>
-					</div>
-				</form>
-			<?php } ?>
 		</div>
 
 	</div>
