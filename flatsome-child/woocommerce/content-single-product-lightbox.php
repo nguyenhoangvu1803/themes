89551e967a271e7639503283e3f9c87ef5cf1732
vubjn <nguyenhoangvu1803@gmail.com>
2020-10-15 14:50:39 +0700

    up

--- flatsome-child/woocommerce/content-single-product-lightbox.php
+++ flatsome-child/woocommerce/content-single-product-lightbox.php
@@ -12,8 +12,8 @@ global $post, $product;
 do_action( 'wc_quick_view_before_single_product' );
 ?>
 <div class="product-quick-view-container">
-	<div class="product-quick-head inner-padding">
-		<h3 class="left">Item Details</h3>
+	<div class="product-quick-head pb-half pt-half">
+		<h3 class="left mb-0">Item Details</h3>
 		<?php 
 			echo apply_filters('flatsome_lightbox_close_button', '<button title="Close" type="button" class="mfp-close right"><svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg></button>');
 		?>
