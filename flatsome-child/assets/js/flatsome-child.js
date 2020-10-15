0892f80caee9c3b2934d9c08bc0f392b26920872
vubjn <nguyenhoangvu1803@gmail.com>
2020-10-15 10:56:52 +0700

    slide

--- flatsome-child/assets/js/flatsome-child.js
+++ flatsome-child/assets/js/flatsome-child.js
@@ -2,20 +2,17 @@ jQuery(document).ready(function($) {
 
 	var $carouselNav = $('.slider-nav');
 
-	console.log(2);
-
 	$(document).on('click', '.nav-cell', function(e) {
 	  var index = $( e.currentTarget ).index();
-	  console.log(index);
-	  console.log(3);
+	  $('.product-lightbox .product-gallery .slider').flickity( 'select', index );
 	});
 
-	$carouselNav.on( 'click', '.nav-cell', function( event ) {
-		console.log(1);
-	  	var index = $( event.currentTarget ).index();
-		console.log(index);
-	  	$('.product-lightbox .product-gallery .slider').flickity( 'select', index );
-	});
+	// $carouselNav.on( 'click', '.nav-cell', function( event ) {
+	// 	console.log(1);
+	//   	var index = $( event.currentTarget ).index();
+	// 	console.log(index);
+	//   	$('.product-lightbox .product-gallery .slider').flickity( 'select', index );
+	// });
 
 	// $('.product-lightbox .product-gallery .slider').flickity('select', 3);
 
