772790537137d613780cdeed1d2e3ce0efdfe950
vubjn <nguyenhoangvu1803@gmail.com>
2020-10-15 10:51:48 +0700

    slide

--- flatsome-child/assets/js/flatsome-child.js
+++ flatsome-child/assets/js/flatsome-child.js
@@ -1,10 +1,11 @@
 jQuery(document).ready(function($) {
 
 	var $carouselNav = $('.slider-nav');
-	var $carouselNavCells = $carouselNav.find('.nav-cell');
 
 	$carouselNav.on( 'click', '.nav-cell', function( event ) {
+		console.log(1);
 	  	var index = $( event.currentTarget ).index();
+		console.log(index);
 	  	$('.product-lightbox .product-gallery .slider').flickity( 'select', index );
 	});
 
