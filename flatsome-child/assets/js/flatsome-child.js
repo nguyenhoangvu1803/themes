6e187a88869d2e29a0625ceb4eb6e333b10f1321
vubjn <nguyenhoangvu1803@gmail.com>
2020-10-16 16:09:46 +0700

    up

--- flatsome-child/assets/js/flatsome-child.js
+++ flatsome-child/assets/js/flatsome-child.js
@@ -12,17 +12,11 @@ jQuery(document).ready(function($) {
 		$(this).parent().find('input[type="search"]').focus();;
 	})
 
-	$('.single-product.slider-nav').flickity({
-	  asNavFor: '.slider',
-	  contain: true,
-	  pageDots: false
+	$('.slider-nav').flickity({
+	  	asNavFor: '.slider',
+	  	contain: true,
+	  	pageDots: false,
+	  	prevNextButtons: false
 	});
-
-	var sliderNav = $('.single-product.slider-nav');
-
-	sliderNav.on('click', '.nav-cell', function(e) {
-		var index = $( e.currentTarget ).index();
-		console.log(index);
-	})
 
 })
