2f7648178480cb7a98fad120d8cb72cfb5ebe8dc
vubjn <nguyenhoangvu1803@gmail.com>
2020-10-16 10:54:52 +0700

    css

--- flatsome-child/functions.php
+++ flatsome-child/functions.php
@@ -543,6 +543,49 @@ add_action( 'woocommerce_after_shop_loop', 'woocommerce_after_shop_loop_end_wrap
 
 // Add ShortCode 
 function login_page() {
+  ?>
+    <div class="account-container lightbox-inner">
+        <div class="account-login-inner">
+
+          <h3 class="uppercase"><?php esc_html_e( 'Log In', 'woocommerce' ); ?></h3>
+
+          <form class="woocommerce-form woocommerce-form-login login" method="post">
+
+            <?php do_action( 'woocommerce_login_form_start' ); ?>
+
+            <p class="woocommerce-form-row woocommerce-form-row--wide form-row form-row-wide">
+              <label for="username"><?php esc_html_e( 'Username or email address', 'woocommerce' ); ?>&nbsp;<span class="required">*</span></label>
+              <input type="text" class="woocommerce-Input woocommerce-Input--text input-text" name="username" id="username" placeholder="Your email..." autocomplete="username" value="<?php echo ( ! empty( $_POST['username'] ) ) ? esc_attr( wp_unslash( $_POST['username'] ) ) : ''; ?>" /><?php // @codingStandardsIgnoreLine ?>
+            </p>
+            <p class="woocommerce-form-row woocommerce-form-row--wide form-row form-row-wide">
+              <label for="password"><?php esc_html_e( 'Password', 'woocommerce' ); ?>&nbsp;<span class="required">*</span></label>
+              <input class="woocommerce-Input woocommerce-Input--text input-text" type="password" name="password" id="password" placeholder="Your first name..." autocomplete="current-password" />
+            </p>
+
+            <?php do_action( 'woocommerce_login_form' ); ?>
+
+            <p class="form-row remember-and-lost">
+              <label class="woocommerce-form__label woocommerce-form__label-for-checkbox woocommerce-form-login__rememberme pull-left">
+                <input class="woocommerce-form__input woocommerce-form__input-checkbox" name="rememberme" type="checkbox" id="rememberme" value="forever" /> <span><?php esc_html_e( 'Remember me', 'woocommerce' ); ?></span>
+              </label>
+              <a class="woocommerce-LostPassword lost_password pull-right" href="<?php echo esc_url( wp_lostpassword_url() ); ?>"><?php esc_html_e( 'Lost your password?', 'woocommerce' ); ?></a>
+            </p>
+            <p class="form-row">
+              <?php wp_nonce_field( 'woocommerce-login', 'woocommerce-login-nonce' ); ?>
+              <button type="submit" class="woocommerce-button button woocommerce-form-login__submit" name="login" value="<?php esc_attr_e( 'Log in', 'woocommerce' ); ?>"><?php esc_html_e( 'Log in', 'woocommerce' ); ?></button>
+            </p>
+
+            <?php do_action( 'woocommerce_login_form_end' ); ?>
+
+          </form>
+
+        </div>
+
+        <div class="text-center review">
+          <span>100.000+ flags sold in Flagwix last year</span>
+        </div>
+    </div>
+  <?php
   return 'LOGIN PAGE';
 }
 
