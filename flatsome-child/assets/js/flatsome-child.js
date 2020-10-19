5a4e0a434e1787d3979451762ddd3a9bb977de4b
vubjn <nguyenhoangvu1803@gmail.com>
2020-10-19 09:26:08 +0700

    css

--- flatsome-child/assets/js/flatsome-child.js
+++ flatsome-child/assets/js/flatsome-child.js
@@ -29,7 +29,7 @@ jQuery(document).ready(function($) {
 
 	// $('.slider-nav').flickity('select');
 
-	var $carousel = $('.slider').flickity({
+	$('.slider-nav').flickity({
 	  on: {
 	    ready: function() {
 	      console.log('Flickity is ready');
