0af435ab17bba88877134f97d0ff10aabbbe7fff
DESKTOP-9BOOVPI\Admin <nguyenhoangvu1803@gmail.com>
2020-10-23 17:01:37 +0700

    up

--- flatsome-child/woocommerce/order/order-details.php
+++ flatsome-child/woocommerce/order/order-details.php
@@ -18,6 +18,11 @@
 defined( 'ABSPATH' ) || exit;
 
 $order = wc_get_order( $order_id ); // phpcs:ignore WordPress.WP.GlobalVariablesOverride.OverrideProhibited
+
+$order_total = $order['order_total']['value'];
+
+echo $order_total;
+
 function filter_woocommerce_get_order_item_totals( $array, $order, $b ) {
 	$ar = [];
 	$ar['cart_subtotal']['value'] = wc_price($order->get_subtotal());
